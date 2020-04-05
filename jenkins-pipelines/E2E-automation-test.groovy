9adaa35f864368530f8ba164606a03f0a805ba5b
Prabhu4tx <38173471+Prabhu4tx@users.noreply.github.com>
2020-04-04 19:12:24 -0500

    Update E2E-automation-test.groovy

--- jenkins-pipelines/E2E-automation-test.groovy
+++ jenkins-pipelines/E2E-automation-test.groovy
@@ -5,7 +5,7 @@ node {
     }
     stage('Smoke') {
         try {
-            sh "maven clean verify -Dtags='type:Smoke'"
+            sh "mvn clean verify -Dtags='type:Smoke'"
         } catch (err) {
 
         } finally {
